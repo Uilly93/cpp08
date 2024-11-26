ed0b021f1995577cf62bb2a9f8df6bb0589b07cd
wnocchi <wnocchi@student.42.fr>
2024-11-26 11:43:52 +0100

    corrected ex00 algorithm

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -1,16 +1,15 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
 #include <stdexcept>
 
-template <typename T> unsigned int easyfind(T &container, int to_find) {
-  unsigned int i = 0;
-  for (typename T::iterator it = container.begin(); it != container.end();
-       it++, i++) {
-    if (*it == to_find)
-      return i;
-  }
-  throw std::invalid_argument("argument not found");
+template <typename T>
+typename T::iterator easyfind(T &container, int to_find) {
+	typename T::iterator it = std::find(container.begin(), container.end(), to_find);
+	if (*it != to_find)
+		throw std::invalid_argument("argument not found");
+	return it;
 }
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,115 +6,108 @@
 #include <vector>
 
 int main(void) {
-  try {
-    std::vector<int> vec;
-    vec.push_back(0);
-    vec.push_back(10);
-    vec.push_back(20);
-    vec.push_back(30);
-    vec.push_back(40);
-    vec.push_back(50);
-    vec.push_back(60);
-    vec.push_back(70);
-    vec.push_back(80);
-    vec.push_back(90);
-    std::cout
-        << NPURPLE
-        << "--------------------------------------------------------------"
-        << RESET << std::endl;
-    std::cout << NGREEN << "		std::vector tests" << RESET
-              << std::endl;
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
-      std::cout << NGREEN << "[" << *it << "] " << RESET;
-    }
-    std::cout
-        << std::endl
-        << NPURPLE
-        << "--------------------------------------------------------------"
-        << RESET << std::endl;
-    int to_find = 40;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << "th position" << RESET
-              << std::endl;
-    to_find = 34;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << " position" << RESET
-              << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  try {
-    std::list<int> vec;
-    vec.push_back(0);
-    vec.push_back(100);
-    vec.push_back(200);
-    vec.push_back(300);
-    vec.push_back(400);
-    vec.push_back(500);
-    vec.push_back(600);
-    vec.push_back(700);
-    vec.push_back(800);
-    vec.push_back(900);
-    std::cout
-        << NPURPLE
-        << "--------------------------------------------------------------"
-        << RESET << std::endl;
-    std::cout << NGREEN << "		     std::list tests" << RESET
-              << std::endl;
-    for (std::list<int>::iterator it = vec.begin(); it != vec.end(); it++) {
-      std::cout << NGREEN << "[" << *it << "] " << RESET;
-    }
-    std::cout
-        << std::endl
-        << NPURPLE
-        << "--------------------------------------------------------------"
-        << RESET << std::endl;
-    int to_find = 100;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << "st position" << RESET
-              << std::endl;
-    to_find = 536;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << " position" << RESET
-              << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
-  try {
-    std::set<int> vec;
-    vec.insert(0);
-    vec.insert(1000);
-    vec.insert(2000);
-    vec.insert(3000);
-    vec.insert(4000);
-    vec.insert(5000);
-    vec.insert(6000);
-    vec.insert(7000);
-    vec.insert(8000);
-    vec.insert(9000);
-    std::cout
-        << NPURPLE
-        << "-------------------------------------------------------------------"
-        << RESET << std::endl;
-    std::cout << NGREEN << "			std::set tests" << RESET
-              << std::endl;
-    for (std::set<int>::iterator it = vec.begin(); it != vec.end(); it++) {
-      std::cout << NGREEN << "[" << *it << "] " << RESET;
-    }
-    std::cout
-        << std::endl
-        << NPURPLE
-        << "-------------------------------------------------------------------"
-        << RESET << std::endl;
-    int to_find = 9000;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << "th position" << RESET
-              << std::endl;
-    to_find = 12535;
-    std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
-    std::cout << GREEN << easyfind(vec, to_find) << " position" << RESET
-              << std::endl;
-  } catch (std::exception &e) {
-    std::cout << RED << e.what() << RESET << std::endl;
-  }
+	try {
+		std::vector<int> vec;
+		vec.push_back(0);
+		vec.push_back(10);
+		vec.push_back(20);
+		vec.push_back(30);
+		vec.push_back(40);
+		vec.push_back(50);
+		vec.push_back(60);
+		vec.push_back(70);
+		vec.push_back(80);
+		vec.push_back(90);
+		std::cout << NPURPLE << "--------------------------------------------------------------"
+				  << RESET << std::endl;
+		std::cout << NGREEN << "		std::vector tests" << RESET << std::endl;
+		for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+			std::cout << NGREEN << "[" << *it << "] " << RESET;
+		}
+		std::cout << std::endl
+				  << NPURPLE << "--------------------------------------------------------------"
+				  << RESET << std::endl;
+		int to_find = 40;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::vector<int>::iterator founded = easyfind(vec, to_find);
+		int i = 0;
+		for (std::vector<int>::iterator it = vec.begin(); it != founded; it++)
+			i++;
+		std::cout << GREEN << i << "th position" << RESET << std::endl;
+		to_find = 34;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::cout << GREEN << *easyfind(vec, to_find) << " position" << RESET << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
+	try {
+		std::list<int> vec;
+		vec.push_back(0);
+		vec.push_back(100);
+		vec.push_back(200);
+		vec.push_back(300);
+		vec.push_back(400);
+		vec.push_back(500);
+		vec.push_back(600);
+		vec.push_back(700);
+		vec.push_back(800);
+		vec.push_back(900);
+		std::cout << NPURPLE << "--------------------------------------------------------------"
+				  << RESET << std::endl;
+		std::cout << NGREEN << "		     std::list tests" << RESET << std::endl;
+		for (std::list<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+			std::cout << NGREEN << "[" << *it << "] " << RESET;
+		}
+		std::cout << std::endl
+				  << NPURPLE << "--------------------------------------------------------------"
+				  << RESET << std::endl;
+		int to_find = 100;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::list<int>::iterator founded = easyfind(vec, to_find);
+		int i = 0;
+		for (std::list<int>::iterator it = vec.begin(); it != founded; it++)
+			i++;
+		std::cout << GREEN << i << "st position" << RESET << std::endl;
+		to_find = 536;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::cout << GREEN << *easyfind(vec, to_find) << " position" << RESET << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
+	try {
+		std::set<int> vec;
+		vec.insert(0);
+		vec.insert(1000);
+		vec.insert(2000);
+		vec.insert(3000);
+		vec.insert(4000);
+		vec.insert(5000);
+		vec.insert(6000);
+		vec.insert(7000);
+		vec.insert(8000);
+		vec.insert(9000);
+		std::cout << NPURPLE
+				  << "-------------------------------------------------------------------" << RESET
+				  << std::endl;
+		std::cout << NGREEN << "			std::set tests" << RESET << std::endl;
+		for (std::set<int>::iterator it = vec.begin(); it != vec.end(); it++) {
+			std::cout << NGREEN << "[" << *it << "] " << RESET;
+		}
+		std::cout << std::endl
+				  << NPURPLE
+				  << "-------------------------------------------------------------------" << RESET
+				  << std::endl;
+		int to_find = 9000;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::set<int>::iterator founded = easyfind(vec, to_find);
+		int i = 0;
+		for (std::set<int>::iterator it = vec.begin(); it != founded; it++)
+			i++;
+		std::cout << GREEN << i << "th position" << RESET << std::endl;
+		to_find = 12535;
+		std::cout << NBLUE << "triyng to find " << to_find << RESET << std::endl;
+		std::cout << GREEN << *easyfind(vec, to_find) << " position" << RESET << std::endl;
+	} catch (std::exception &e) {
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
 }
